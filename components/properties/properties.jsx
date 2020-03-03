012d5149479a4078c5e8df4b3b28f6483d5870c7
yvens47 <yvens47@yahoo.com>
2020-03-03 16:28:40 -0500

    3 files added

---
+++ components/properties/properties.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+class Login extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 mt-5 pt-5 m-auto">
+            property
+            
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Login;
