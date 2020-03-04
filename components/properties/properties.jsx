5280a54f9c9d9e9f3408846d64992fe3deadfd9c
yvens47 <yvens47@yahoo.com>
2020-03-04 13:29:07 -0500

    2 files modified

--- components/properties/properties.jsx
+++ components/properties/properties.jsx
@@ -7,6 +7,9 @@ class Properties extends React.Component {
         <div className="row mt-5">
           <div className="map_list">sffs</div>
           <div className="col-md-6 mt-5 pt-5 m-auto">property</div>
+          <div className='col-md-6'>
+            
+          </div>
         </div>
       </div>
     );
